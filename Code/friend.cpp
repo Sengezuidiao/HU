e5efff08798084f8a9fd6612d8d7e4ca696ad8c5
qinguansen <1454538956@qq.com>
2023-05-08 11:43:28 +0800

    add all

---
+++ Code/friend.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+//友元成员函数
+class Building
+{
+	friend void MyFriend::LookAtBedRoom(Building& building);
+
+}
+class MyFriend
+{
+public:
+	void LookAtBedRoom(Building& building);	
+	void PlayInBedRoom(Building& building);
+
+
+}
+
+
+
+
+
+
+
+int main()
+{
+
+
+	return 0;
+}
+
