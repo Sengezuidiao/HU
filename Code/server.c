737b23313966673048491ba04e44f030a51a865f
qinguansen <1454538956@qq.com>
2023-05-31 18:42:58 +0800

    add all

--- Code/server.c
+++ Code/server.c
@@ -8,6 +8,7 @@ int main()
 {
 	WORD wVersionRequested;
 	WSADATA wsaData;
+	int iResult = 0;
 	int err;
 	wVersionRequested = MAKEWORD(2, 2); //winsock 版本2.2
 	err = WSAStartup(wVersionRequested, &wsaData);
@@ -18,15 +19,24 @@ int main()
 	}
 	else
 		printf("The Winsock 2.2 dll was fould\n");
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if(sock == INVALID_SOCKET)
+	SOCKET servSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if(servSock == INVALID_SOCKET)
 		printf("socket function failed with error = %d\n", WSAGetLastError());
 	SOCKADDR_IN sockAddr;
 	memset(&sockAddr,0,sizeof(sockAddr));
 	sockAddr.sin_family = PF_INET;
 	sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	sockAddr.sin_port = htons(1234);
-	bind(servSock, &sockAddr, sizeof(sockAddr));
+
+	iResult = bind(servSock, (SOCKADDR*)&sockAddr, sizeof(sockAddr));
+	if(iResult == SOCKET_ERROR)
+	{
+		printf("bind failed with error %u\n", WSAGetLastError());
+		return 1;
+	}
+	listen(servSock, 20);
+	char *str = "connect success!\n";
+	send(servSock, str, strlen(str), MSG_OOB);
 	
 	WSACleanup();
 	return 0;
